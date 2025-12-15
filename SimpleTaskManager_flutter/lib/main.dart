bd9a7c6b48d432ad791ee766595e56ebbf8fba0d
seifwael3 <131828719+seifwael3@users.noreply.github.com>
2025-12-15 08:19:18 +0200

    Create main.dart

---
+++ SimpleTaskManager_flutter/lib/main.dart
@@ -0,0 +1,76 @@
+import 'import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const TaskScreen(),
+    );
+  }
+}
+
+class TaskScreen extends StatefulWidget {
+  const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  final List<String> tasks = [];
+  final TextEditingController controller = TextEditingController();
+
+  void addTask() {
+    if (controller.text.isNotEmpty) {
+      setState(() {
+        tasks.add(controller.text);
+        controller.clear();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Task Manager'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                labelText: 'Enter task',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: addTask,
+              child: const Text('Add Task'),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(tasks[index]),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
