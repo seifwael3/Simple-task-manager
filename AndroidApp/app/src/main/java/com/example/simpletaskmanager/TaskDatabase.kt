3357d6b94f3c350e0dcfa908ce1743bb2c4f1b8e
seifwael3 <131828719+seifwael3@users.noreply.github.com>
2025-12-15 08:39:44 +0200

    Create TaskDatabase.kt

---
+++ AndroidApp/app/src/main/java/com/example/simpletaskmanager/TaskDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.simpletaskmanager.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).allowMainThreadQueries().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
