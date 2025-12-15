9fe60747990720cc05eae0af305c94859343c9ce
seifwael3 <131828719+seifwael3@users.noreply.github.com>
2025-12-15 08:33:35 +0200

    Create Task.kt

---
+++ AndroidApp/app/src/main/java/com/example/simpletaskmanager/Task.kt
@@ -0,0 +1,11 @@
+package com.example.simpletaskmanager
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String
+)
